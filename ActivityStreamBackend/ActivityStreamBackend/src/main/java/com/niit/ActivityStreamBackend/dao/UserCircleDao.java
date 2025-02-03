0dc5060ba22a36c23ccdb203f93d8fa6fe2bc0ab
govindahedau <70361069+govindahedau@users.noreply.github.com>
2025-02-03 16:17:09 +0530

    Delete ActivityStreamBackend directory

--- ActivityStreamBackend/ActivityStreamBackend/src/main/java/com/niit/ActivityStreamBackend/dao/UserCircleDao.java
+++
@@ -1,8 +0,0 @@
-package com.niit.ActivityStreamBackend.dao;
-import java.util.List;
-import com.niit.ActivityStreamBackend.model.UserCircle;
-public interface UserCircleDao {
-	boolean addUserToCircle(String emailId, String circleName);
-	boolean deleteUserfromCircle(String emailId, String circleName);
-	List <String> getUsersofCircle(String circleName);
-}
